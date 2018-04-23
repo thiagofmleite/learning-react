e16e05bbfac87140e3db39471b758243679e135e
Thiago Leite <thiago.f.leite@accenture.com>
2018-04-23 17:34:49 -0300

    Corrigindo as rotas com a nova versão

--- src/App.js
+++ src/App.js
@@ -9,32 +9,27 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div id="layout">
-          <a href="#menu" id="menuLink" className="menu-link">
-            <span></span>
-          </a>
+      <div id="layout">
+        <a href="#menu" id="menuLink" className="menu-link">
+          <span></span>
+        </a>
 
-          <div id="menu">
-            <div className="pure-menu">
-              <a className="pure-menu-heading" href="/">Company</a>
+        <div id="menu">
+          <div className="pure-menu">
+            <a className="pure-menu-heading" href="/">Company</a>
 
-              <ul className="pure-menu-list">
-                <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
-                <li className="pure-menu-item"><Link to="/autor" className="pure-menu-link">Autor</Link></li>
-                <li className="pure-menu-item"><Link to="/livro" className="pure-menu-link">Livro</Link></li>
-              </ul>
-            </div>
-          </div>
-
-          <div id="main">
-            <Route exact path="/" component={Home} />
-            <div className="content" id="content">
-              <Route path="/autor" component={AutorBox} />
-            </div>
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
+              <li className="pure-menu-item"><Link to="/autor" className="pure-menu-link">Autor</Link></li>
+              <li className="pure-menu-item"><Link to="/livro" className="pure-menu-link">Livro</Link></li>
+            </ul>
           </div>
         </div>
-      </Router>
+
+        <div id="main">
+          {this.props.children}
+        </div>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Home from './Home';
+import AutorBox from './Autor'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/autor" component={AutorBox} />
+                {/* <Route path="/livro" component={LivroBox} /> */}
+            </Switch>
+        </App>
+    </Router>,
+    document.getElementById('root')
 );
